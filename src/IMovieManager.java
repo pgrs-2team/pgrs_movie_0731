98c680409d3c89f174ff967e31c25dec740d4092
power-minu <mw0677@naver.com>
2024-08-01 14:41:52 +0900

    init

---
+++ src/IMovieManager.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IMovieManager {
+    void addMovie(Movie movie); // 영화 등록
+
+    void deleteMovie(int id); // 영화 삭제
+
+    void updateMovie(int id, LocalDate startDate, LocalDate endDate); // 영화 수정(날짜만 가능)
+
+    Movie getMovie(String title); //제목으로 영화 검색
+
+    List<Movie> movieList(); //영화 전체 목록
+
+    void closeConn();
+}
---
+++ src/IReservationManager.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public interface IReservationManager {
+    void createReservation(Reservation reservation); // 예약 하기
+
+    Reservation getReservation(String userName); // 이름으로 예약 검색
+
+    List<Reservation> reservationList(); //예약 전체 목록
+
+    void deleteReservation(int id); // 예약 삭제
+
+    void updateReservation(int id, int headCount); // 예약 수정 (인원 수만)
+
+    void closeConn();
+}
